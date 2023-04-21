81fb6118575059907dd761445cdaa9d3e52fcb68
Ankit Singh Rathore <ankit.singh.rathore@accenture.com>
2023-04-21 14:30:45 +0530

    Route setup + Fonts added + Load fonts first then load app

--- app/index.js
+++ app/index.js
@@ -1,34 +1,34 @@
-import { StyleSheet, Text, View } from "react-native";
+import {SafeAreaView, View, ScrollView} from 'react-native';
+import { Stack , useRouter } from 'expo-router';
+import {COLORS, icons, images, SIZES} from '../constants';
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components'
 
-export default function Page() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.main}>
-        <Text style={styles.title}>Hello World</Text>
-        <Text style={styles.subtitle}>This is the first page of your app.</Text>
-      </View>
-    </View>
-  );
+const Home = () => {
+    const router = useRouter();
+    return (
+        <SafeAreaView style= {{flex:1, backgroundColor: COLORS.lightWhite}}>
+            <Stack.Screen 
+                options={{
+                    headerStyle : {backgroundColor: COLORS.lightWhite },
+                    headerShadowVisible: false,
+                    headerLeft: () =>(
+                        <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+                    ),
+                    headerRight: () => (
+                        <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
+                    ),
+                    headerTitle: ''
+                }}
+            />
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View style= {{flex:1, padding:SIZES.medium}}>
+                    <Welcome />
+                    <Popularjobs />
+                    <Nearbyjobs />
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+    )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    padding: 24,
-  },
-  main: {
-    flex: 1,
-    justifyContent: "center",
-    maxWidth: 960,
-    marginHorizontal: "auto",
-  },
-  title: {
-    fontSize: 64,
-    fontWeight: "bold",
-  },
-  subtitle: {
-    fontSize: 36,
-    color: "#38434D",
-  },
-});
+export default Home
