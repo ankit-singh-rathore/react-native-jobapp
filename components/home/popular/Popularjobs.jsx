68fb2dac6281721709a10067b46dae9907651591
Ankit Singh Rathore <ankit.singh.rathore@accenture.com>
2023-04-24 10:25:06 +0530

    Custom useFech HOOK - used for dynamic api calling

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -6,9 +6,22 @@ import styles from "./popularjobs.style";
 import { COLORS, SIZES } from "../../../constants";
 import { ActivityIndicator } from "react-native-web";
 
+import useFetch from '../../../hooks/useFetch';
+
 const Popularjobs = () => {
-  const isLoading = false;
-  const error = false;
+
+const { isLoading, error , data} = useFetch (
+  'search',
+  {
+    query : "React developer",
+    num_pages: 1
+  }
+)
+
+console.log("DDD", data);
+
+  // const isLoading = false;    -- isLoading and error will now come dynamically from usefetch hook (above)
+  // const error = false;
   return (
     <View style={styles.container}>
       <View style={styles.header}>
---
+++ hooks/useFetch.js
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+
+const useFetch = (endpoint, query) => {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(false);
+  const [isloading, setIsloading] = useState(false);
+
+  const options = {
+    method: "GET",
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    headers: {
+      "X-RapidAPI-Key": 'bbb4db950bmsh12d98fc87928e59p117345jsn7e7e3955408c',
+      "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+    },
+    params: { ...query },
+  };
+
+  const fetchData = async () => {
+    setIsloading(true);
+    
+    try{
+      const res = await axios.request(options);
+      setData(res.data.data);
+      setIsloading(false);
+    }
+    catch(error) {
+      console.log(error);
+    }
+    finally{
+      setIsloading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const reFetch = () => {
+    fetchData();
+  }
+  return { data, isloading, error, reFetch}
+}
+
+export default useFetch
